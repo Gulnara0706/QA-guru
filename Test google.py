cc9e33feba05ace588ef92f259f2d070d9219766
79818 <gulnara0706@yandex.ru>
2022-06-17 09:22:10 +0300

    Add requirements.txt, Test google.py

--- Test google.py
+++ Test google.py
@@ -4,7 +4,7 @@ from selene.browser import element
 from selene.support.shared import browser
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(autouse=True)
 def browser():
     browser.open(https // www.google.ru )
     browser.set_window_size(1920, 1080)
@@ -15,6 +15,7 @@ def positive_test(browser):
     press_enter()
     browser.element('[id=search]')
     should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+    print("ок")
 
 
 
@@ -25,6 +26,7 @@ def nagative_test(browser):
     press_enter()
     browser.element('[id=search]')
     should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+    print("ок")
 
 
 
